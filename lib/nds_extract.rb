6d8ce03af5420ef5c013cd6658ea03e75c40d4d2
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-14 17:55:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,8 @@ end
 def gross_for_director(director_data)
   total = 0
   i = 0
-  while index < director_data[:movies][i][:worldwide_gross]
+  while index < director_data[:movies].length do
+    total += director_data[:movies][i][:worldwide_grosses]
   i += 1
 end
 total
